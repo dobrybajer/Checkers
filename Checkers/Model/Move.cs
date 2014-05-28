7a66a3281e3522261c6b86f60d91b4f5ec08f974
dobrybajer <kamil.zakk@gmail.com>
2014-05-28 17:17:35 +0200

    missing files

---
+++ Checkers/Model/Move.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers.Model
+{
+    public class Move
+    {
+        private int from;
+
+        private int to;
+
+        public Move(int moveFrom, int moveTo)
+        {
+            from = moveFrom;
+            to = moveTo;
+        }
+
+        public int getFrom()
+        {
+            return from;
+        }
+
+        public int getTo()
+        {
+            return to;
+        }
+
+        public override string ToString()
+        {
+            return "(" + from + "," + to + ")";
+        }
+    }
+}
